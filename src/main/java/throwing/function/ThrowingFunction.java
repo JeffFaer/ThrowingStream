a7bc67dd4da33db9807d58a8d51c4971ab58255d
Jeffrey Falgout <jeffrey.falgout@gmail.com>
2014-04-18 15:21:44 -0400

    Added a similar andThen to ThrowingFunction

--- src/main/java/throwing/function/ThrowingFunction.java
+++ src/main/java/throwing/function/ThrowingFunction.java
@@ -1,6 +1,13 @@
 package throwing.function;
 
+import java.util.Objects;
+
 @FunctionalInterface
 public interface ThrowingFunction<T, R, X extends Throwable> {
     public R apply(T t) throws X;
+    
+    default public <RR> ThrowingFunction<T, RR, X> andThen(ThrowingFunction<? super R, ? extends RR, ? extends X> after) {
+        Objects.requireNonNull(after);
+        return t -> after.apply(apply(t));
+    }
 }
