97c613389bc290f6c61ddff921412f49d3c240a9
Jeffrey Falgout <jeffrey.falgout@gmail.com>
2014-04-20 00:39:59 -0400

    Exceptions thrown are now much less verbose. All of the bridging details are filtered out of the stack trace elements so that the only information left is the invocation on the steam where the stream is evaluated and the location (probably in a lambda) where the exception was thrown along the pipeline.

--- src/main/java/throwing/bridge/CheckedBridge.java
+++ src/main/java/throwing/bridge/CheckedBridge.java
@@ -1,6 +1,11 @@
 package throwing.bridge;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.IntSummaryStatistics;
+import java.util.List;
 import java.util.function.Supplier;
+import java.util.stream.IntStream;
 
 abstract class CheckedBridge<D, X extends Throwable> extends AbstractBridge<D> {
     private final FunctionBridge<X> bridge;
@@ -28,6 +33,24 @@ abstract class CheckedBridge<D, X extends Throwable> extends AbstractBridge<D> {
             if (t instanceof BridgeException) {
                 Throwable cause = t.getCause();
                 if (getBridge().getExceptionClass().isInstance(cause)) {
+                    // filter out bridge lines from the exception. They can get
+                    // rather verbose.
+                    List<StackTraceElement> ste = Arrays.asList(cause.getStackTrace());
+                    IntSummaryStatistics stats = IntStream.range(0, ste.size())
+                            .filter(i -> ste.get(i).getClassName().startsWith("throwing.bridge"))
+                            .summaryStatistics();
+                    if (stats.getCount() > 0) {
+                        List<StackTraceElement> filtered = new ArrayList<>(ste.size()
+                                - (stats.getMax() - stats.getMin()));
+                        for (int x = 0; x < stats.getMin(); x++) {
+                            filtered.add(ste.get(x));
+                        }
+                        for (int x = stats.getMax() + 1; x < ste.size(); x++) {
+                            filtered.add(ste.get(x));
+                        }
+                        cause.setStackTrace(filtered.toArray(new StackTraceElement[filtered.size()]));
+                    }
+                    
                     throw getBridge().getExceptionClass().cast(cause);
                 } else {
                     throw t;
--- src/test/java/throwing/stream/ThrowingStreamTest.java
+++ src/test/java/throwing/stream/ThrowingStreamTest.java
@@ -1,12 +1,17 @@
 package throwing.stream;
 
+import static org.hamcrest.Matchers.hasItemInArray;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.startsWith;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import org.junit.Test;
 
@@ -48,7 +53,7 @@ public class ThrowingStreamTest {
     }
     
     @Test
-    public void flatMapExceptionsAreHandledCorrectly() {
+    public void flatMapExceptions() {
         ThrowingIntStream<Exception> s = ThrowingBridge.of(numbers, Exception.class);
         
         List<Integer> collected = new ArrayList<>();
@@ -56,6 +61,7 @@ public class ThrowingStreamTest {
         try {
             s.flatMap(i -> {
                 if (i >= 10) {
+                    e.fillInStackTrace();
                     throw e;
                 }
                 return ThrowingBridge.of(IntStream.of(i), Exception.class);
@@ -66,5 +72,41 @@ public class ThrowingStreamTest {
         }
         
         assertEquals(10, collected.size());
+    }
+    
+    @Test
+    public void flatMapExceptionsInStream() {
+        ThrowingIntStream<Exception> s = ThrowingBridge.of(numbers, Exception.class);
+        
+        List<Integer> collected = new ArrayList<>();
+        Exception e = new Exception();
+        try {
+            s.flatMap(i -> ThrowingBridge.of(IntStream.of(i), Exception.class).peek(i2 -> {
+                if (i2 >= 10) {
+                    e.fillInStackTrace();
+                    throw e;
+                }
+            })).forEach(collected::add);
+            fail();
+        } catch (Exception e2) {
+            assertSame(e, e2);
+        }
+        
+        assertEquals(10, collected.size());
+    }
+    
+    @Test
+    public void exceptionsAreNotVerbose() {
+        ThrowingIntStream<Exception> s = ThrowingBridge.of(numbers, Exception.class);
+        s = s.peek(i -> {
+            throw new Exception();
+        });
+        try {
+            s.count();
+            fail();
+        } catch (Exception e) {
+            assertThat(Stream.of(e.getStackTrace()).map(StackTraceElement::getClassName).toArray(String[]::new),
+                    not(hasItemInArray(startsWith("throwing.bridge"))));
+        }
     }
 }
