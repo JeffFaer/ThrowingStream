95b588f94ace0657db1cec589e49fb1625cd491b
Jeffrey Falgout <jeffrey.falgout@gmail.com>
2015-03-12 16:13:23 -0400

    Make the stack trace filtering a bit more flexible

--- src/main/java/throwing/bridge/CheckedBridge.java
+++ src/main/java/throwing/bridge/CheckedBridge.java
@@ -8,24 +8,28 @@ import java.util.function.Supplier;
 import java.util.stream.IntStream;
 
 abstract class CheckedBridge<D, X extends Throwable> extends AbstractBridge<D> {
+    private static final String PACKAGE = CheckedBridge.class.getPackage().getName();
+    // it might be useful to have a way to turn this off
+    public static volatile boolean FILTER_STACK = true;
+
     private final FunctionBridge<X> bridge;
-    
+
     CheckedBridge(D delegate, FunctionBridge<X> bridge) {
         super(delegate);
         this.bridge = bridge;
     }
-    
+
     protected FunctionBridge<X> getBridge() {
         return bridge;
     }
-    
+
     protected void filterBridgeException(Runnable runnable) throws X {
         filterBridgeException(() -> {
             runnable.run();
             return null;
         });
     }
-    
+
     protected <R> R filterBridgeException(Supplier<R> supplier) throws X {
         try {
             return supplier.get();
@@ -35,22 +39,10 @@ abstract class CheckedBridge<D, X extends Throwable> extends AbstractBridge<D> {
                 if (getBridge().getExceptionClass().isInstance(cause)) {
                     // filter out bridge lines from the exception. They can get
                     // rather verbose.
-                    List<StackTraceElement> ste = Arrays.asList(cause.getStackTrace());
-                    IntSummaryStatistics stats = IntStream.range(0, ste.size())
-                            .filter(i -> ste.get(i).getClassName().startsWith("throwing.bridge"))
-                            .summaryStatistics();
-                    if (stats.getCount() > 0) {
-                        List<StackTraceElement> filtered = new ArrayList<>(ste.size()
-                                - (stats.getMax() - stats.getMin()));
-                        for (int x = 0; x < stats.getMin(); x++) {
-                            filtered.add(ste.get(x));
-                        }
-                        for (int x = stats.getMax() + 1; x < ste.size(); x++) {
-                            filtered.add(ste.get(x));
-                        }
-                        cause.setStackTrace(filtered.toArray(new StackTraceElement[filtered.size()]));
+                    if (FILTER_STACK) {
+                        filterStackTrace(cause);
                     }
-                    
+
                     throw getBridge().getExceptionClass().cast(cause);
                 } else {
                     throw t;
@@ -62,6 +54,23 @@ abstract class CheckedBridge<D, X extends Throwable> extends AbstractBridge<D> {
             } else {
                 throw new AssertionError(t);
             }
+        }
+    }
+
+    // this method also filters out a lot of the java.util.stream trace elements
+    private void filterStackTrace(Throwable cause) {
+        List<StackTraceElement> ste = Arrays.asList(cause.getStackTrace());
+        IntSummaryStatistics stats = IntStream.range(0, ste.size()).filter(
+                i -> ste.get(i).getClassName().startsWith(PACKAGE)).summaryStatistics();
+        if (stats.getCount() > 0) {
+            List<StackTraceElement> filtered = new ArrayList<>(ste.size() - (stats.getMax() - stats.getMin()));
+            for (int x = 0; x < stats.getMin(); x++) {
+                filtered.add(ste.get(x));
+            }
+            for (int x = stats.getMax() + 1; x < ste.size(); x++) {
+                filtered.add(ste.get(x));
+            }
+            cause.setStackTrace(filtered.toArray(new StackTraceElement[filtered.size()]));
         }
     }
 }
