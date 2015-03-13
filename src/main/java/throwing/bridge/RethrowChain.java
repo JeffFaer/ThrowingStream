bbc82691030cf3f7d274f09fa70ae0ef4166ca1f
Jeffrey Falgout <jeffrey.falgout@gmail.com>
2015-03-13 16:48:16 -0400

    Make rethrow chain a Function

--- src/main/java/throwing/bridge/RethrowChain.java
+++ src/main/java/throwing/bridge/RethrowChain.java
@@ -4,7 +4,7 @@ import java.util.Optional;
 import java.util.function.Function;
 
 @FunctionalInterface
-interface RethrowChain<X extends Throwable, Y extends Throwable> {
+interface RethrowChain<X extends Throwable, Y extends Throwable> extends Function<X, Optional<Y>> {
     public static final RethrowChain<Throwable, Throwable> START = t -> Optional.empty();
     public static final RethrowChain<Throwable, Throwable> END = t -> {
         if (t instanceof Error) {
@@ -26,20 +26,19 @@ interface RethrowChain<X extends Throwable, Y extends Throwable> {
         return (RethrowChain<X, Y>) END;
     }
 
-    public Optional<Y> handle(X t);
-
     default public <Z extends Throwable> RethrowChain<X, Z> rethrow(Function<? super Y, ? extends Z> mapper) {
-        return t -> this.handle(t).map(mapper);
+        return t -> apply(t).map(mapper);
     }
 
     default public RethrowChain<X, Y> chain(RethrowChain<X, Y> second) {
         return this == START ? second : t -> {
-            Optional<Y> o = this.handle(t);
-            return o.isPresent() ? o : second.handle(t);
+            Optional<Y> o = apply(t);
+            return o.isPresent() ? o : second.apply(t);
         };
     };
 
     default public Function<X, Y> finish() {
-        return t -> this.chain(end()).handle(t).get();
+        RethrowChain<X, Y> c = this.chain(end());
+        return t -> c.apply(t).get();
     }
 }
