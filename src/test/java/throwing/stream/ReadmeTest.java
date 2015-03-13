01f3bd460085f3126bb4f1aae8f070849bb5b99e
Jeffrey Falgout <jeffrey.falgout@gmail.com>
2015-03-13 18:55:39 -0400

    Update readme test

--- src/test/java/throwing/stream/ReadmeTest.java
+++ src/test/java/throwing/stream/ReadmeTest.java
@@ -9,14 +9,12 @@ import java.util.stream.Stream;
 
 import org.junit.Test;
 
-import throwing.bridge.ThrowingBridge;
-
 public class ReadmeTest {
     @SuppressWarnings("unchecked")
     @Test
     public void readme() throws ClassNotFoundException {
         Stream<String> names = Stream.of("java.lang.Object", "java.util.stream.Stream");
-        ThrowingStream<String, ClassNotFoundException> s = ThrowingBridge.of(names, ClassNotFoundException.class);
+        ThrowingStream<String, ClassNotFoundException> s = ThrowingStream.of(names, ClassNotFoundException.class);
         List<Class<?>> l = s.map(ClassLoader.getSystemClassLoader()::loadClass).collect(toList());
         assertThat(l, contains(Object.class, Stream.class));
     }
